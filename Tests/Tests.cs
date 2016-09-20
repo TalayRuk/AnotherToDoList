5bc1edad6f948e7bb4ecdd0ef69cfd7eb4c8f1b3
Jonathan Buchner and vichitra  pool <jonathan+vichitra@>
2016-09-20 11:34:25 -0700

    add getAll, deleteAll

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -14,15 +14,22 @@ namespace Tests
     public Testing()
     {
       string dataSource = "Data Source=(localdb)\\mssqllocaldb"; // Data Source identifies the server.
-      string dbName = "database"; // Initial Catalog is the database name
+      string dbName = "todo_test"; // Initial Catalog is the database name
       //Integrated Security sets the security of the database access to the Windows user that is currently logged in.
       DBConfiguration.ConnectionString = dataSource+";Initial Catalog="+dbName+";Integrated Security=SSPI;";
     }
 
+    [Fact]
+    public void Test1_AreTestWork_True()
+    {
+      Task testTask = new Task("a");
+
+      Assert.Equal("a", testTask.GetDescription());
+    }
 
     public void Dispose()
     {
-      // Item.DeleteAll();
+      Task.DeleteAll();
     }
   }
 }
