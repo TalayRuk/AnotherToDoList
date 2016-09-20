4d4acdf5470f78925a7be9202f78847070c1cee0
Jonathan Buchner and vichitra  pool <jonathan+vichitra@>
2016-09-20 15:28:43 -0700

    add find() for task

---
+++ Objects/Task.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using ToDo;
+
+namespace ToDo.Objects
+{
+  public class Task
+  {
+    private int _id;
+    private string _description;
+
+    public Task(string Description, int Id = 0)
+    {
+      _id = Id;
+      _description = Description;
+    }
+
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public void SetId(int newId)
+    {
+      _id = newId;
+    }
+    //Instance Methods
+
+    //Static Methods
+    //Get All Rows from
+    public static List<Task> GetAll()
+    {
+      List<Task> allList = new List<Task> {};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      string statement = "SELECT * FROM tasks;";
+      SqlCommand cmd = new SqlCommand (statement, conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read() )
+      {
+        int id = rdr.GetInt32(0);
+        string description = rdr.GetString(1);
+        Task tempTask = new Task (description, id);
+        allList.Add(tempTask);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allList;
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      string statement = "INSERT INTO tasks (description) OUTPUT INSERTED.id VALUES (@Task);";
+      SqlCommand cmd = new SqlCommand (statement, conn);
+      SqlParameter dp = new SqlParameter();
+      dp.ParameterName = "@Task";
+      dp.Value = this.GetDescription();
+      cmd.Parameters.Add(dp);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while ( rdr.Read() )
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static Task Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      string statement = "SELECT * FROM tasks WHERE id = @TaskId;";
+      SqlCommand cmd = new SqlCommand(statement, conn);
+      SqlParameter taskId = new SqlParameter();
+      taskId.ParameterName = "@TaskId";
+      taskId.Value = id.ToString();
+      cmd.Parameters.Add(taskId);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      int foundTaskId = 0;
+      string foundTaskDescription = null;
+
+      while (rdr.Read())
+      {
+        foundTaskId = rdr.GetInt32(0);
+        foundTaskDescription = rdr.Getstring(1);
+      }
+      Task foundTask = new Task(foundTaskDescription, foundTaskId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+      
+      return foundTask;
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      string line = "DELETE FROM tasks;";
+      SqlCommand cmd = new SqlCommand(line,conn);
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+    }
+    //Overrides
+    public override bool Equals(System.Object otherTask)
+    {
+      if (!(otherTask is Task)) {
+        return false;
+      }
+      else
+      {
+        Task newTask = (Task) otherTask;
+        bool descriptionEquality = (this.GetDescription() == newTask.GetDescription());
+        bool idEquality = (this.GetId() == newTask.GetId());
+        return (descriptionEquality && idEquality);
+      }
+    }
+    public override int GetHashCode()
+    {
+      return this.GetDescription().GetHashCode();
+    }
+
+  }
+}
---
+++ Tests/TaskTests.cs
@@ -0,0 +1,84 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using ToDo.Objects;
+using ToDo;
+
+
+namespace ToDo
+{
+  public class TaskTest : IDisposable
+  {
+    public TaskTest()
+    {
+      string dataSource = "Data Source=(localdb)\\mssqllocaldb"; // Data Source identifies the server.
+      string dbName = "todo_test"; // Initial Catalog is the database name
+      //Integrated Security sets the security of the database access to the Windows user that is currently logged in.
+      DBConfiguration.ConnectionString = dataSource+";Initial Catalog="+dbName+";Integrated Security=SSPI;";
+    }
+    [Fact]
+    public void Test1_AreTestWork_true()
+    {
+      Task testTask = new Task("a");
+
+      Assert.Equal("a", testTask.GetDescription());
+    }
+    [Fact]
+    public void Test2_IsTheDatabaseEmpty()
+    {
+     int AmountOfRows = Task.GetAll().Count;
+     Assert.Equal(0, AmountOfRows);
+    }
+    [Fact]
+    public void Test3_IsDataOverRideWorks()
+    {
+      Task firstTask = new Task ("any");
+      Task secondTask = new Task ("any");
+
+      Assert.Equal (firstTask, secondTask);
+    }
+    [Fact]
+    public void Test4_CanWeSave()
+    {
+      //Arrange
+      Task testTask = new Task("Do laundry");
+      //Act
+      testTask.Save();
+      //Assert
+      List<Task> saved = Task.GetAll();
+      List<Task> notsaved = new List<Task>{testTask};
+      Assert.Equal(notsaved, saved);
+    }
+    [Fact]
+    public void Test5_SaveOneItem()
+    {
+      //Arrange
+      Task testTask = new Task("Update Computers");
+      //Act
+      testTask.Save();
+      //Assert
+      Task savedTask = Task.GetAll()[0];
+      int result = savedTask.GetId();
+      int testId = testTask.GetId();
+      Assert.Equal(result, testId);
+    }
+    [Fact]
+    public void Test6_Find_TaskInDatabase()
+    {
+      //Arrange
+      Tesk testTask = new Task("start code")
+      //Act
+      testTask.Save();
+      //Assert
+      Task foundTask = Task.Find(testTask.GetId());
+      Assert.Equal(foundTask, testTask);
+    }
+
+    public void Dispose()
+    {
+      Task.DeleteAll();
+    }
+  }
+}
